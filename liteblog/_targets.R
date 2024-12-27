90d08b9a72314ea3bdafdcf7c6d9d4e23d3e8439
Danielle Navarro <djnavarro@protonmail.com>
2024-12-27 12:17:36 +1100

    use dynamic branching in targets

--- liteblog/_targets.R
+++ liteblog/_targets.R
@@ -1,25 +1,9 @@
 library(targets)
 tar_source(files = "liteblog.R")
-pages <- page_paths(get_options())
-target_page <- function(page, ind) {
-  file_target <- paste0("page_", ind)
-  fuse_target <- paste0("fuse_", ind)
-  fuse_cmd <- paste0("fuse_page(", file_target, ", opt)")
-  list(
-    tar_target_raw(
-      name = file_target,
-      command = page,
-      format = "file"
-    ),
-    tar_target_raw(
-      name = fuse_target,
-      command = str2lang(fuse_cmd),
-    )
-  )
-}
 list(
   tar_target(opt, get_options()),
   tar_target(page_list, page_paths(opt)),
-  unlist(purrr::imap(pages, target_page)),
+  tar_target(page, page_list, pattern = map(page_list), format = "file"),
+  tar_target(fuse, fuse_page(page), pattern = map(page)),
   tar_target(static, copy_static(opt))
 )
