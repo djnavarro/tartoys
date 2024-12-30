57cbc4572ab8d201ad56e003eb76c61338b335e4
Danielle Navarro <djnavarro@protonmail.com>
2024-12-31 10:55:28 +1100

    store blog config/functions as an R6 object

--- liteblog/_liteblog.R
+++ liteblog/_liteblog.R
@@ -1,134 +1,139 @@
-liteblog_options <- function() {
+Liteblog <- R6::R6Class(
+  classname = "Liteblog",
+  public = list(
 
-  # setup
-  opt <- list()
-  opt$root   <- rprojroot::find_root(rprojroot::has_file("_liteblog.R"))
-  opt$source <- "source"
-  opt$output <- "site"
-  opt$css    <- "_liteblog.css"
+    initialize = function(root, source, output, css) {
+      self$root <- root
+      self$source <- source
+      self$output <- output
+      self$css <- css
+    },
 
-  # list blog posts
-  opt$post_file_list <- function(opt) {
-    pages <- fs::dir_ls(
-      path = opt$source,
-      recurse = TRUE,
-      regexp = "[.][rR]?md$"
-    )
-    unname(unclass(pages))
-  }
+    root = NULL,
+    source = NULL,
+    output = NULL,
+    css = NULL,
+    pattern = "[.][rR]?md$",
 
-  # fuse post
-  opt$post_file_fuse <- function(page, opt) {
-    post_output <- litedown::fuse(page)
-    post_output_file <- fs::path_file(post_output)
-    if (post_output_file %in% c("index.html", "404.html")) {
-      site_output <- paste0(opt$output, "/", post_output_file)
-    } else {
-      site_output <- post_output_file |>
-        stringr::str_replace_all("_", "/") |>
-        stringr::str_replace("\\.html$", "/index.html") |>
-        stringr::str_replace("^", paste0(opt$output, "/"))
-    }
-    site_output <- fs::path(opt$root, site_output)
-    fs::dir_create(fs::path_dir(site_output))
-    fs::file_move(post_output, site_output)
-  }
+    find_posts = function() {
+      files <- fs::dir_ls(
+        path = fs::path(self$root, self$source),
+        recurse = TRUE,
+        regexp = self$pattern
+      )
+      unname(unclass(files))
+    },
 
-  # list static files
-  opt$static_file_list <- function(opt) {
-    files <- fs::dir_ls(
-      path = fs::path(opt$root, opt$source),
-      recurse = TRUE,
-      regexp = "[.][rR]?md$",
-      invert = TRUE
-    )
-    unname(unclass(files))
-  }
+    find_static = function() {
+      files <- fs::dir_ls(
+        path = fs::path(self$root, self$source),
+        recurse = TRUE,
+        regexp = self$pattern,
+        invert = TRUE
+      )
+      unname(unclass(files))
+    },
 
-  # copt static files
-  opt$static_file_copy <- function(file, opt) {
-    fs::dir_create(fs::path(opt$root, opt$output))
-    fs::file_copy(
-      file,
-      file |> stringr::str_replace(
-        pattern = paste0("/", opt$source, "/"),
-        replacement = paste0("/", opt$output, "/")
-      ),
-      overwrite = TRUE
-    )
-  }
-
-  # required for front page post listing
-  opt$post_list <- function() {
-    post_dir <- fs::path(opt$root, opt$source)
-    post <- post_dir |>
-      fs::dir_ls(regexp = "[.][rR]?md$") |>
-      fs::path_rel(post_dir) |>
-      stringr::str_remove("^_") |>
-      stringr::str_replace_all("_", "/") |>
-      fs::path_ext_remove() |>
-      setdiff(c("index", "404")) |>
-      stringr::str_replace("$", "/index.html")
-    purrr::walk(
-      rev(post),
-      \(p) {
-        t <- p |>
-          fs::path_dir() |>
-          fs::path_split() |>
-          unlist() |>
-          paste(collapse = " / ")
-        l <- paste0("[", t, "](", p, ")")
-        cat("-", l, "\n")
+    fuse_post = function(file) {
+      output_path <- litedown::fuse(file)
+      output_file <- fs::path_file(output_path)
+      if (stringr::str_detect(output_file, "^_")) {
+        destination <- output_file |>
+          stringr::str_replace_all("_", "/") |>
+          stringr::str_replace("\\.html$", "/index.html") |>
+          stringr::str_replace("^", paste0(self$output, "/"))
+      } else {
+        destination <- paste0(self$output, "/", output_file)
       }
-    )
-  }
+      destination <- fs::path(self$root, destination)
+      fs::dir_create(fs::path_dir(destination))
+      fs::file_move(output_path, destination)
+    },
 
-  # used on most pages
-  opt$footer <- function(depth = 0) {
-    home <- paste(rep("..", depth), collapse = "/")
-    if (depth == 0) home <- "./"
-    cat(
-      "<br><br>",
-      paste0("<a href=\"", home, "/\">←</a>"),
-      sep = "\n"
-    )
-  }
+    copy_static = function(file) {
+      fs::dir_create(fs::path(self$root, self$output))
+      destination <- file |>
+        stringr::str_replace(
+          pattern = paste0("/", self$source, "/"),
+          replacement = paste0("/", self$output, "/")
+        )
+      fs::file_copy(
+        path = file,
+        new_path = destination,
+        overwrite = TRUE
+      )
+    },
 
-  # user convenience
-  opt$new_post <- function(file) {
-    out <- fs::path(opt$root, opt$source, file)
-    depth <- length(stringr::str_extract_all(file, "_")[[1]])
-    rmd <- c(
-      "---",
-      paste("title: ", file),
-      "output:",
-      "  litedown::html_format:",
-      "    meta:",
-      "      css: [\"default\", \"../_liteblog.css\"]",
-      "options:",
-      "  toc: true",
-      "---",
-      "",
-      "```{r}",
-      "#| label: setup",
-      "#| echo: false",
-      "source(\"../_liteblog.R\")",
-      "```",
-      "",
-      "Text",
-      "",
-      "```{r}",
-      "#| label: footer",
-      "#| echo: false",
-      "#| results: asis",
-      paste0(".blog$footer(depth = ", depth, ")"),
-      "```",
-      ""
-    )
-    fs::dir_create(fs::path(opt$root, opt$source))
-    brio::write_lines(rmd, out)
-  }
+    write_post_list = function() {
+      post_dir <- fs::path(self$root, self$source)
+      post <- post_dir |>
+        fs::dir_ls(regexp = self$pattern) |>
+        fs::path_rel(post_dir) |>
+        stringr::str_subset("^_") |>
+        stringr::str_remove("^_") |>
+        stringr::str_replace_all("_", "/") |>
+        fs::path_ext_remove() |>
+        stringr::str_replace("$", "/index.html")
+      purrr::walk(
+        rev(post),
+        \(p) {
+          t <- p |>
+            fs::path_dir() |>
+            fs::path_split() |>
+            unlist() |>
+            paste(collapse = " / ")
+          l <- paste0("[", t, "](", p, ")")
+          cat("-", l, "\n")
+        }
+      )
 
-  opt
-}
+    },
+
+    write_footer = function(depth = 0) {
+      home <- paste(rep("..", depth), collapse = "/")
+      if (depth == 0) home <- "./"
+      cat(
+        "<br><br>",
+        paste0("<a href=\"", home, "/\">←</a>"),
+        sep = "\n"
+      )
+    }
+  )
+)
+
+#
+#   # user convenience
+#   new_blog_post <- function(file) {
+#     out <- fs::path(opt$root, opt$source, file)
+#     depth <- length(stringr::str_extract_all(file, "_")[[1]])
+#     rmd <- c(
+#       "---",
+#       paste("title: ", file),
+#       "output:",
+#       "  litedown::html_format:",
+#       "    meta:",
+#       "      css: [\"default\", \"../_liteblog.css\"]",
+#       "options:",
+#       "  toc: true",
+#       "---",
+#       "",
+#       "```{r}",
+#       "#| label: setup",
+#       "#| echo: false",
+#       "source(\"../_liteblog.R\")",
+#       "```",
+#       "",
+#       "Text",
+#       "",
+#       "```{r}",
+#       "#| label: footer",
+#       "#| echo: false",
+#       "#| results: asis",
+#       paste0(".blog$footer(depth = ", depth, ")"),
+#       "```",
+#       ""
+#     )
+#     fs::dir_create(fs::path(opt$root, opt$source))
+#     brio::write_lines(rmd, out)
+#   }
 
--- liteblog/_targets.R
+++ liteblog/_targets.R
@@ -1,17 +1,22 @@
 library(targets)
 
 tar_source("_liteblog.R")
+root  <- rprojroot::find_root(rprojroot::has_file("_liteblog.R"))
 
 list(
-  tar_target(opt, liteblog_options()),
+  # blog configuration
+  tar_target(blog, Liteblog$new(root, "source", "site", "_liteblog.css")),
+  tar_target(blog_rds, saveRDS(blog, file = "_liteblog.rds"), format = "file"),
 
-  # build targets for site pages
-  tar_target(post_list, opt$post_file_list(opt)),
+  # detect file paths
+  tar_target(post_list, blog$find_posts()),
+  tar_target(static_list, blog$find_static()),
+
+  # specify file targets
   tar_target(post, post_list, pattern = map(post_list), format = "file"),
-  tar_target(fuse, opt$post_file_fuse(post, opt), pattern = map(post)),
+  tar_target(static, static_list, pattern = map(static_list), format = "file"),
 
-  # build targets for static files
-  tar_target(file_list, opt$static_file_list(opt)),
-  tar_target(file, file_list, pattern = map(file_list), format = "file"),
-  tar_target(copy, opt$static_file_copy(file, opt), pattern = map(file))
+  # fuse/copy targets
+  tar_target(fuse, blog$fuse_post(post), pattern = map(post)),
+  tar_target(copy, blog$copy_static(static), pattern = map(static))
 )
