e0db8263a35669ce4e38841ceb4a69d680a735af
Danielle Navarro <djnavarro@protonmail.com>
2024-12-30 14:17:47 +1100

    move build functions into _targets.R

--- liteblog/_targets.R
+++ liteblog/_targets.R
@@ -1,5 +1,54 @@
 library(targets)
-tar_source("scripts/build.R")
+
+root <- rprojroot::find_root(rprojroot::has_file("_liteblog.yml"))
+
+post_file_list <- function(root, opt) {
+  pages <- fs::dir_ls(
+    path = fs::path(root, opt$source),
+    recurse = TRUE,
+    regexp = "[.][rR]?md$"
+  )
+  unname(unclass(pages))
+}
+
+post_file_fuse <- function(page, root, opt) {
+  post_output <- litedown::fuse(page)
+  post_output_file <- fs::path_file(post_output)
+  if (post_output_file %in% c("index.html", "404.html")) {
+    site_output <- paste0(opt$output, "/", post_output_file)
+  } else {
+    site_output <- post_output_file |>
+      stringr::str_replace_all("_", "/") |>
+      stringr::str_replace("\\.html$", "/index.html") |>
+      stringr::str_replace("^", paste0(opt$output, "/"))
+  }
+  site_output <- fs::path(root, site_output)
+  fs::dir_create(fs::path_dir(site_output))
+  fs::file_move(post_output, site_output)
+}
+
+static_file_list <- function(root, opt) {
+  files <- fs::dir_ls(
+    path = fs::path(root, opt$source),
+    recurse = TRUE,
+    regexp = "[.][rR]?md$",
+    invert = TRUE
+  )
+  unname(unclass(files))
+}
+
+static_file_copy <- function(file, root, opt) {
+  fs::dir_create(fs::path(root, opt$output))
+  fs::file_copy(
+    file,
+    file |> stringr::str_replace(
+      pattern = paste0("/", opt$source, "/"),
+      replacement = paste0("/", opt$output, "/")
+    ),
+    overwrite = TRUE
+  )
+}
+
 list(
   # track and read the options file
   tar_target(opt_file, "_liteblog.yml", format = "file"),
