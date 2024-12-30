fa3f1cb4b3ebe32e2003b445008122079ae356aa
Danielle Navarro <djnavarro@protonmail.com>
2024-12-30 15:10:59 +1100

    encapsulate tools in .blog

--- liteblog/blog/_blog.R
+++ liteblog/blog/_blog.R
@@ -1,8 +1,11 @@
+# store everything in a list
 .blog <- list()
 
+# project root and configuration
 .blog$root <- rprojroot::find_root(rprojroot::has_file("_liteblog.yml"))
 .blog$opt <- yaml::read_yaml(fs::path(.blog$root, "_liteblog.yml"))
 
+# required for front page post listing
 .blog$post_list <- function() {
   post_dir <- fs::path(.blog$root, .blog$opt$source)
   post <- post_dir |>
@@ -27,6 +30,7 @@
   )
 }
 
+# used on most pages
 .blog$footer <- function(depth = 0) {
   home <- paste(rep("..", depth), collapse = "/")
   cat(
@@ -35,5 +39,42 @@
     sep = "\n"
   )
 }
+
+# user convenience
+.blog$new_post <- function(file) {
+  dir <- fs::path(.blog$root, .blog$opt$source)
+  out <- fs::path(dir, file)
+  depth <- length(stringr::str_extract_all(file, "_")[[1]])
+  rmd <- c(
+    "---",
+    paste("title: ", file),
+    "output:",
+    "  litedown::html_format:",
+    "    meta:",
+    "      css: [\"default\", \"_blog.css\"]",
+    "options:",
+    "  toc: true",
+    "---",
+    "",
+    "```{r}",
+    "#| label: setup",
+    "#| echo: false",
+    "source(\"_blog.R\")",
+    "```",
+    "",
+    "Text",
+    "",
+    "```{r}",
+    "#| label: footer",
+    "#| echo: false",
+    "#| results: asis",
+    paste0(".blog$footer(depth = ", depth, ")"),
+    "```",
+    ""
+  )
+  fs::dir_create(dir)
+  brio::write_lines(rmd, out)
+}
+
 
 
