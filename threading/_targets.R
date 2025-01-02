76ec75dfae9a831104a7e0ba4ec45684b824984b
Danielle Navarro <djnavarro@protonmail.com>
2025-01-03 09:28:37 +1100

    threads report start and stop time

--- threading/_targets.R
+++ threading/_targets.R
@@ -4,9 +4,21 @@ library(crew)
 
 tar_option_set(controller = crew_controller_local(workers = 3))
 
+sleeper <- function(duration, pipeline_start) {
+  sleep_start <- Sys.time()
+  Sys.sleep(duration)
+  sleep_stop <- Sys.time()
+  data.frame(
+    start = round(difftime(sleep_start, pipeline_start), digits = 3),
+    stop  = round(difftime(sleep_stop, pipeline_start), digits = 3)
+  )
+}
+
 list(
-  tar_target(wait1, Sys.sleep(1)),
-  tar_target(wait2, Sys.sleep(2)),
-  tar_target(wait3, Sys.sleep(3)),
-  tar_target(wait4, Sys.sleep(4))
+  tar_target(start, Sys.time(), cue = tar_cue("always")),
+  tar_target(wait1, sleeper(1, start)),
+  tar_target(wait2, sleeper(2, start)),
+  tar_target(wait3, sleeper(3, start)),
+  tar_target(wait4, sleeper(4, start)),
+  tar_target(waits, rbind(wait1, wait2, wait3, wait4))
 )
